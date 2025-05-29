ef1925eb9347a614ea2d9e17562bdc92ab298355
Aimeri <edntremblay@gmail.com>
2025-05-29 06:10:42 -0400

    Update config.lua
    Added the Config.NPCShop and Config.RewardCurrency flags with their descriptions.

--- gb-skimmer/config.lua
+++ gb-skimmer/config.lua
@@ -1,5 +1,6 @@
 Config = {}
 
+Config.NPCShop = true -- True if you want the built in shop to sell the skimmer with built in purchase cooldown.  False if you want to handle how the skimmer is distributed on your own.
 Config.Npc = {
     model = 's_m_y_dealer_01',
     coords = vector4(489.1, -1521.77, 29.29, 133.15)
@@ -10,6 +11,7 @@ Config.SkimmerItem = 'skimmer'
 Config.SkimmerUseTime = 20 * 60 * 1000
 Config.SkimmerPurchaseCooldown = 45 * 60 * 1000
 
+Config.RewardCurrency = 'black_money' -- markedbills if you want to be rewarded with markedbills with a worth assigned to them.  black_money if you want the reward to be black_money currency.
 Config.Reward = {
     min = 5000,
     max = 15000
