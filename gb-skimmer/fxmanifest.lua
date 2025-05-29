34ba3dd69854901cd06e4ae3168e73772abfb81c
Aimeri <edntremblay@gmail.com>
2025-05-29 06:09:58 -0400

    Update fxmanifest.lua
    Just added the ui_page and files sections to support the new NUI timer display.

--- gb-skimmer/fxmanifest.lua
+++ gb-skimmer/fxmanifest.lua
@@ -5,6 +5,14 @@ description 'ATM Skimmer Script'
 author 'GrossBean'
 version '1.0.0'
 
+ui_page 'html/index.html'
+
+files {
+  'html/index.html',
+  'html/style.css',
+  'html/script.js'
+}
+
 shared_script 'config.lua'
 
 client_script 'client/main.lua'
